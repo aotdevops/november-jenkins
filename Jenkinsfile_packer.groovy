5dff43fc3b19bc587fb4e310ddfa6f3c8f4b1aa6
ahmed tekin <ahmedtekin@gmail.com>
2021-03-22 17:20:50 -0500

    Update Jenkinsfile_packer.groovy

--- Jenkinsfile_packer.groovy
+++ Jenkinsfile_packer.groovy
@@ -8,7 +8,7 @@ node {
 				'elk', 
 				'gitlab',
 				'source_ami_filter',
-				'jenkins']
+				'jenkins'],
 		description: 'What would you like to build? ', 
 		name: 'TOOL'), 
 			choice(choices: 
